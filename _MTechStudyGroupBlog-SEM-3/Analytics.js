f38db9e3d903053e414df67e35af6a1879e9db2e
ManojKumarM75 <114361477+ManojKumarM75@users.noreply.github.com>
2023-10-22 10:46:23 +0400

    Delete _MTechStudyGroupBlog-SEM-3 directory

--- _MTechStudyGroupBlog-SEM-3/Analytics.js
+++
@@ -1,9 +0,0 @@
-/* Analytics */
-function Main_analytics{
-    async src="https://www.googletagmanager.com/gtag/js?id=G-2W0V3GSXXM";
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2W0V3GSXXM');
-}
